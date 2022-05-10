713b1870265124c781fe6ca79cec0cbaa64d291e
Дмитрий Никифоров <dnikiforov@innoseti.ru>
2022-05-10 14:49:53 +0200

    fix: fixed FuctionComponent interface, removed useless css in app, moved '*.d.ts' files to 'types' folder

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import "./app.module.css";
 
 export default class App extends React.Component {
   render() {
