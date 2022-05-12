dde2a5c2afe9900b59a83fb17ba1d253bf13b19f
Дмитрий Никифоров <dnikiforov@innoseti.ru>
2022-05-12 16:08:12 +0200

    feat: added api and loader

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -4,14 +4,19 @@ import AppHeader from '../app-header/app-header';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 
-import { INGREDIENTS } from '../../constants';
 import { ingredients } from '../../interfaces/ingredients';
 
 const App = () => {
   const [ingredients, setIngredients] = useState<ingredients.ingredient[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    setIngredients(INGREDIENTS);
+    setIsLoading(true);
+    fetch('https://norma.nomoreparties.space/api/ingredients', { method: 'GET' })
+      .then((result) => result.json())
+      .then((result) => setIngredients(result.data))
+      .catch((e) => console.error(e))
+      .finally(() => setIsLoading(false));
   }, []);
 
   const [offset, setOffset] = useState(0);
@@ -20,7 +25,9 @@ const App = () => {
     setOffset(offset);
   }, []);
 
-  return (
+  return isLoading ? (
+    <div className="loading" />
+  ) : (
     <>
       <AppHeader changeOffset={changeOffset} />
       <main className="flex container jc-center" style={{ height: `calc(100vh - ${offset}px)`, margin: '0 auto' }}>
