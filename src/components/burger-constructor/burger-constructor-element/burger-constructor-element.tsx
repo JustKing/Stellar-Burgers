5ddcb898986a92c802028279129fa42c56b03acf
Дмитрий Никифоров <dnikiforov@innoseti.ru>
2022-05-14 19:59:29 +0200

    fix: added additional text to top and bottom buns in BurgerConstructorElement component, updated info in package.json

--- src/components/burger-constructor/burger-constructor-element/burger-constructor-element.tsx
+++ src/components/burger-constructor/burger-constructor-element/burger-constructor-element.tsx
@@ -1,4 +1,4 @@
-import { memo, CSSProperties } from 'react';
+import { memo, CSSProperties, useMemo } from 'react';
 
 import { DragIcon, ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
 
@@ -12,12 +12,23 @@ type Props = {
 };
 
 const BurgerConstructorElement = memo(({ ingredient, isLocked, type, style }: Props) => {
+  const getTitle = useMemo(() => {
+    if (type === 'top') {
+      return `${ingredient.name} (верх)`;
+    }
+    if (type === 'bottom') {
+      return `${ingredient.name} (низ)`;
+    }
+
+    return ingredient.name;
+  }, [ingredient, type]);
+
   return (
     <div className={`${isLocked ? (ingredient.type === 'bottom' ? 'mt-2' : 'mb-2') : ''} flex ai-center`} style={style}>
       <span className={`${isLocked ? 'mr-8' : 'mr-2'}`}>{!isLocked && <DragIcon type="primary" />}</span>
       <ConstructorElement
         type={type}
-        text={ingredient.name}
+        text={getTitle}
         price={ingredient.price}
         thumbnail={ingredient.image}
         isLocked={isLocked}
